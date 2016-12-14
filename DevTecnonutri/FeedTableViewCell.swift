0282ca993811dddda0159cbd69475f8dff2a904b
Thales Frigo <thalesfrigo@gmail.com>
2016-12-14 20:11:13 -0200

    Create FeedViewController and FeedTableViewCell

---
+++ DevTecnonutri/FeedTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  FeedTableViewCell.swift
+//  DevTecnonutri
+//
+//  Created by Thales Frigo on 12/14/16.
+//  Copyright © 2016 Thales Frigo. All rights reserved.
+//
+
+import UIKit
+
+class FeedTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
