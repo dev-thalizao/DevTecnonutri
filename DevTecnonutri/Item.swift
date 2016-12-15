ba37bcc101623494d3038d7a105cac87a0c7896c
Thales Frigo <thalesfrigo@gmail.com>
2016-12-15 01:03:08 -0200

    Created a model related to feed (only the necessary)

---
+++ DevTecnonutri/Item.swift
@@ -0,0 +1,17 @@
+//
+//  Item.swift
+//  DevTecnonutri
+//
+//  Created by Thales Frigo on 12/14/16.
+//  Copyright © 2016 Thales Frigo. All rights reserved.
+//
+
+import Foundation
+
+struct Item {
+    let imageUrl: String
+    let mealType: Meal
+    let date: Date
+    let energy: Float
+    let profile: Profile
+}
---
+++ DevTecnonutri/Meal.swift
@@ -0,0 +1,13 @@
+//
+//  Meal.swift
+//  DevTecnonutri
+//
+//  Created by Thales Frigo on 12/14/16.
+//  Copyright © 2016 Thales Frigo. All rights reserved.
+//
+
+import Foundation
+
+public enum Meal: Int {
+    case breakfast = 0, morningSnack, lunch, afternoonSnack, dinner, supper, preWorkout, postWorkout
+}
