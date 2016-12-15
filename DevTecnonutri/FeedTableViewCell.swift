57deea423ffa7ef3b6199a2eaa575ecd96b554cc
Thales Frigo <thalesfrigo@gmail.com>
2016-12-15 01:06:37 -0200

    Bind cell properties; Connect presenter to the view; Implement FeedViewProtocol

--- DevTecnonutri/FeedTableViewCell.swift
+++ DevTecnonutri/FeedTableViewCell.swift
@@ -10,9 +10,29 @@ import UIKit
 
 class FeedTableViewCell: UITableViewCell {
 
+    
+    @IBOutlet weak var rootView: UIView!
+    
+    @IBOutlet weak var profileImage: UIImageView!
+    @IBOutlet weak var profileName: UILabel!
+    @IBOutlet weak var profileGoal: UILabel!
+    @IBOutlet weak var mealImage: UIImageView!
+    @IBOutlet weak var mealDate: UILabel!
+    @IBOutlet weak var mealEnergy: UILabel!
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
+        rootView.layer.borderWidth = 0.3
+        rootView.layer.borderColor = UIColor.lightGray.cgColor
+        rootView.layer.cornerRadius = 1.33
+        
+        // Circle image
+        profileImage.layer.cornerRadius = profileImage.frame.size.width / 2
+        profileImage.layer.borderWidth = 0.5
+        profileImage.layer.borderColor = UIColor.lightGray.cgColor
+        profileImage.clipsToBounds = true
+        
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
---
+++ DevTecnonutri/LayoutConfig.swift
@@ -0,0 +1,20 @@
+//
+//  LayoutConfig.swift
+//  DevTecnonutri
+//
+//  Created by Thales Frigo on 12/15/16.
+//  Copyright © 2016 Thales Frigo. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class LayoutConfig {
+    // Fonts
+    static let largeFont = UIFont(name: "Helvetica-Light", size: 24.0)
+    static let mediumFont = UIFont(name: "Helvetica-Light", size: 18.0)
+    
+    // Colors
+    static let primaryTextColor = UIColor.init(colorLiteralRed: 118.0, green: 116.0, blue: 120, alpha: 1.0)
+    static let secondaryTextColor = UIColor(red: 227/255, green: 227/225, blue: 228/255, alpha: 1.0)
+}
